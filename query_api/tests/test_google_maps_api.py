a64c5fa3efdb8c8db161d047b0c7f860aa864f5c
vm-ubuntu <bakr.frag@github.com>
2022-06-12 22:10:45 +0200

    add testcase to test set_query_search

--- query_api/tests/test_google_maps_api.py
+++ query_api/tests/test_google_maps_api.py
@@ -11,4 +11,10 @@ class TestQueryGoogleMaps(unittest.TestCase):
         self.url = config("GOOGLE_MAPS_URL")
         self.wrong_key = self.key[0:10]
 
-    
+    def test_set_query_search(self):
+        """
+        test set search query to empty string
+        """
+        google_maps = QueryGoogleMaps()
+        result = google_maps.set_search_query("")
+        self.assertEqual(result, "search query can't be empty")

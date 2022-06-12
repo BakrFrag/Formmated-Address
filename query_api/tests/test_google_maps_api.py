3fcac1df6aa35eed0bfe7d6c3958da957c567c47
vm-ubuntu <bakr.frag@github.com>
2022-06-12 22:13:21 +0200

    add testcases to test extract & formatt address

--- query_api/tests/test_google_maps_api.py
+++ query_api/tests/test_google_maps_api.py
@@ -43,3 +43,35 @@ class TestQueryGoogleMaps(unittest.TestCase):
         self.assertEqual(response.get("error_message"),
                          "Invalid request. Missing the 'address', 'components', 'latlng' or 'place_id' parameter.")
         self.assertFalse(response.get("results"))
+
+    def test_extract_address(self):
+        google_maps = QueryGoogleMaps()
+        google_maps.key = self.key
+
+        google_maps.set_search_query("5 Av. Anatole, Paris, Champ de Mars")
+        results = google_maps.send_query_request()
+
+        self.assertEqual(results, ["2 All. Adrienne Lecouvreur,75007,Paris,France",
+                                   "5 Av. Anatole France,75007,Paris,France"])
+
+        google_maps.set_search_query("St Anthony's Hospital")
+        results = google_maps.send_query_request()
+
+        self.assertEqual(
+            results, "1200 7th Ave N,33705,St. Petersburg,United States")
+
+        google_maps.set_search_query("Croydon united kingdom")
+        results = google_maps.send_query_request()
+
+        self.assertEqual(results, " ,,Croydon,United Kingdom")
+
+        google_maps.set_search_query("Rembrandt House Museum")
+        results = google_maps.send_query_request()
+       
+        self.assertEqual(
+            results, "4 Jodenbreestraat,1011 NK,Amsterdam,Netherlands")
+
+        google_maps.set_search_query("Royal Marsden Hospital - Sutton")
+        results = google_maps.send_query_request()
+       
+        self.assertEqual(results, " Downs Rd,SM2 5PT,,United Kingdom")
